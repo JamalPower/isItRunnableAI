1b61e5640c096adf950eef72ccafb29708abc0f4
Jamal <jamalnainia2007@gmail.com>
2026-05-01 14:14:09 +0100

    pdate

--- api/app.js
+++ api/app.js
@@ -151,5 +151,5 @@ app.use((err, req, res, next) => {
     console.error("Unhandled error:", err);
     res.status(500).json({ error: "Internal server error" });
 });
-//----------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 module.exports = app;
